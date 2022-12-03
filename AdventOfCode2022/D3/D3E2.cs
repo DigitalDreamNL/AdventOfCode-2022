765c9f457f6cee633ba35f452835ae4911adb098
DigitalDreamNL <github@digitaldream.nl>
2022-12-03 11:29:37 +0100

    Update day 3 with LINQ Chunk().

--- AdventOfCode2022/D3/D3E2.cs
+++ AdventOfCode2022/D3/D3E2.cs
@@ -5,37 +5,19 @@ public class D3E2 : D3
     public async Task<string> Execute()
     {
         var groups = await DetermineGroups();
-        groups.ForEach(HandleGroup);
+        groups.ToList().ForEach(HandleGroup);
         return SumOfPriorities.ToString();
     }
 
-    private void HandleGroup(List<string> groups)
+    private void HandleGroup(string[] groups)
     {
         var badge = FindBadge(groups);
         var priority = CalculatePriority(badge);
         SumOfPriorities += priority;
     }
 
-    private static async Task<List<List<string>>> DetermineGroups()
-    {
-        var counter = 0;
-        var group = new List<string>();
-        var groups = new List<List<string>>();
-
-        using var sr = new StreamReader("D3/src/input.txt");
-        while (!sr.EndOfStream)
-        {
-            group.Add((await sr.ReadLineAsync())!);
-            counter++;
-
-            if (counter % 3 != 0)
-                continue;
-
-            groups.Add(group);
-            group = new List<string>();
-        }
-        return groups;
-    }
+    private static async Task<IEnumerable<string[]>> DetermineGroups()
+        => (await File.ReadAllLinesAsync("D3/src/input.txt")).Chunk(3);
 
     private static char FindBadge(IReadOnlyCollection<string> rucksacks) 
         => string.Join("", rucksacks)
